1a24c7d426fc866370cf28232861bdeb5f10e78e
sarautrera <sara.utrerav@edu.uah.es>
2026-03-18 17:52:43 +0100

    Delete src/main/java/es/uah/matcomp/mp/e1/ejerciciosclases/practica3y4 directory

--- src/main/java/es/uah/matcomp/mp/e1/ejerciciosclases/practica3y4/ejercicio63/Movable.java
+++
@@ -1,8 +0,0 @@
-package es.uah.matcomp.mp.e1.ejerciciosclases.practica3y4.ejercicio63;
-
-public interface Movable {
-    public void moveUp();
-    public void moveDown();
-    public void moveLeft();
-    public void moveRight();
-}
