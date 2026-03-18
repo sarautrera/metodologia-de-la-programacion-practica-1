469b6b5eedb933b56f5961d642e6a4df2afbd9a5
EstherLoechesM <estherloeches.m@gmail.com>
2026-03-18 19:21:24 +0100

    P1

---
+++ P1/src/Practica1/test/TestCircle.java
@@ -0,0 +1,33 @@
+package Practica1.test;
+
+public class TestCircle {
+    public static void main(String[] args){
+        //declarar una instancia de la clase circulo llamada c1
+        Circle c1 = new Circle();
+        //Para pedir al objeto que haga algo o te de informacion se utilica el operador (.)
+        //Invocar los metodos publicos en la instancia c1, con el operador (.)
+        System.out.println("El circulo tiene un radio de "
+                + c1.getRadius()+" y un area de "+c1.getArea());
+        Circle c2 = new Circle(2.0);
+        //Invocar los metodos publicos en la instancia c2, con el operador (.)
+        System.out.println("El circulo tiene un radio de "
+                + c2.getRadius()+ " y un area de "+c2.getArea());
+        Circle c4 = new Circle();
+        c4.setRadius(5.5);
+        System.out.println("El radio es "+ c4.getRadius());
+        c4.setColor("verde");
+        System.out.println("El color es "+c4.getColor());
+        //Esto no se puede hacer porque devuelve vacío: System.out.println(c4.setRadius(4.4));
+        //De manera explicita
+        Circle c5 = new Circle(5.5);
+        System.out.println(c5.toString());
+        //De manera implicita
+        Circle c6 = new Circle(6.6);
+        System.out.println(c6.toString());
+        System.out.println(c6);
+        System.out.println("Operator '+' invokes toString() too: " + c6);
+
+
+
+    }
+}
